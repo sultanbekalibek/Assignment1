b65bb47cbd757b02657dea62ae503efb8a3eda8c
sultanbekalibek <sultanbekalibek15@gmail.com>
2025-12-21 15:23:27 +0500

    Sultanbek assignment1

--- MyApplication.java
+++ MyApplication.java
@@ -3,7 +3,6 @@ import models.Shape;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Locale;
